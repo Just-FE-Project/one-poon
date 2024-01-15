2f600a2af32fed02a57e8ea3507c1ce6aab508fa
choi1five <cwo1401@gmail.com>
2024-01-15 16:07:56 +0900

    chore: eslint, prettier, vite 관련 라이브러리 추가 및 적용

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,28 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended"],
-  ignorePatterns: ["dist", ".eslintrc.cjs", "*.html"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['html', '@typescript-eslint', 'simple-import-sort', 'react-refresh'],
   rules: {
-    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'prettier/prettier': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
   },
 };
