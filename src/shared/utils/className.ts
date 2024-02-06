d345f77e1f858c9caf9a4bafdfd68842104849f2
정세훈 <88763715+jeong-se-hun@users.noreply.github.com>
2024-02-06 13:02:13 +0900

    프로젝트 리셋

--- src/shared/utils/className.ts
+++
@@ -1,7 +0,0 @@
-import { cva, type VariantProps } from 'class-variance-authority';
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
-
-export { cva as classVarianceAuthority, cn, VariantProps };
