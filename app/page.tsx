736b3bf182dc9ee5e159076b27d5ac611e0193b1
정세훈 <88763715+jeong-se-hun@users.noreply.github.com>
2024-02-08 00:41:04 +0900

    feat: 인풋,모달,디바이더 컴포넌트 마이그레이션

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
+'use client';
+
 import { Button } from '@/shared/components/button/button';
 import { Loading } from '@/shared/components/loading/loading';
 import { Avatar } from '@/shared/components/avatar/avatar';
 import Badge from '@/shared/components/badge/badge';
+import { useState } from 'react';
+import { Modal } from '@/shared/components/modal/modal';
+import Divider from '@/shared/components/divider/divider';
 
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <main>
       {/* 버튼테스트 */}
       <Button className="btn-primary">Primary</Button>
-      <div className="my-10"></div>
+      <Divider />
       {/* 로딩 스피너 테스트 */}
       <Loading color={'primary'} size={'lg'} />
       <Loading variant={'ball'} color={'secondary'} size={'lg'} />
@@ -17,9 +24,8 @@ export default function Home() {
       <br />
       <Loading variant={'ring'} size={'lg'} />
       <br />
-      <br />
+      <Divider />
       {/* 아바타 테스트 */}
-      <div className="my-10"></div>
 
       <Avatar src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
       <br />
@@ -28,14 +34,30 @@ export default function Home() {
       <Avatar color="secondary" border offline shape="square">
         <span>user2</span>
       </Avatar>
-      {/* 배지 테스트 */}
-      <div className="my-10"></div>
-      <Badge className="badge-secondary">배지</Badge>
+      <Divider />
 
+      {/* 배지 테스트 */}
+      <Badge className="badge-secondary">배지</Badge>
+      <br />
       <Button>
         Inbox
         <Badge className="badge-secondary">배지</Badge>
       </Button>
+      <Divider />
+      {/* 모달 사용 테스트 */}
+      <button className="btn" onClick={() => setIsModalOpen(true)}>
+        모달 열기
+      </button>
+      <Modal isOpen={isModalOpen} position="middle">
+        <Modal.Header>모달 제목</Modal.Header>
+        <Modal.Body>모달 내용</Modal.Body>
+        <Modal.Actions>
+          <button className="btn" onClick={() => setIsModalOpen(false)}>
+            닫기
+          </button>
+        </Modal.Actions>
+      </Modal>
+      <Divider />
     </main>
   );
 }
