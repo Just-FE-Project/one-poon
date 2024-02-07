2000389862b0674eaab4151573258cca049475b2
정세훈 <88763715+jeong-se-hun@users.noreply.github.com>
2024-02-08 00:22:46 +0900

    feat: 배지 컴포넌트를 마이그레이션

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Button } from '@/shared/components/button/button';
 import { Loading } from '@/shared/components/loading/loading';
 import { Avatar } from '@/shared/components/avatar/avatar';
+import Badge from '@/shared/components/badge/badge';
 
 export default function Home() {
   return (
     <main>
       {/* 버튼테스트 */}
       <Button className="btn-primary">Primary</Button>
+      <div className="my-10"></div>
       {/* 로딩 스피너 테스트 */}
       <Loading color={'primary'} size={'lg'} />
       <Loading variant={'ball'} color={'secondary'} size={'lg'} />
@@ -16,7 +18,9 @@ export default function Home() {
       <Loading variant={'ring'} size={'lg'} />
       <br />
       <br />
-      {/* 배지 테스트 */}
+      {/* 아바타 테스트 */}
+      <div className="my-10"></div>
+
       <Avatar src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
       <br />
       <Avatar letters="user1" color="primary" border online shape="circle" />
@@ -24,6 +28,14 @@ export default function Home() {
       <Avatar color="secondary" border offline shape="square">
         <span>user2</span>
       </Avatar>
+      {/* 배지 테스트 */}
+      <div className="my-10"></div>
+      <Badge className="badge-secondary">배지</Badge>
+
+      <Button>
+        Inbox
+        <Badge className="badge-secondary">배지</Badge>
+      </Button>
     </main>
   );
 }
