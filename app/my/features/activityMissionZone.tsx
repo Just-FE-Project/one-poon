2e306c66c9315e230e98809f62c94213b7d11139
정세훈 <88763715+jeong-se-hun@users.noreply.github.com>
2024-03-01 20:11:19 +0900

    feat: 미션 페이지 추가

--- app/my/features/activityMissionZone.tsx
+++ app/my/features/activityMissionZone.tsx
@@ -2,9 +2,15 @@ import Image from 'next/image';
 
 interface IProps {
   title?: string;
+  size?: 'md' | 'lg';
 }
 
-export function ActivityMissionZone({ title }: IProps) {
+export function ActivityMissionZone({ title, size = 'md' }: IProps) {
+  const sizeClass = {
+    md: { imgSize: 50, progressHeight: 'h-6', textSize: 'text-md' },
+    lg: { imgSize: 70, progressHeight: 'h-8', textSize: 'text-lg' }
+  }[size];
+
   return (
     <div className="flex flex-col gap-14 px-6 py-20">
       {title && <h3 className="text-2xl font-bold">{title}</h3>}
@@ -12,12 +18,12 @@ export function ActivityMissionZone({ title }: IProps) {
         <Image
           className="absolute left-[-8px] top-[-32%] z-10"
           src={'/lv1.svg'}
-          width={50}
-          height={50}
+          width={sizeClass.imgSize}
+          height={sizeClass.imgSize}
           alt="lv1 캐릭터"></Image>
         <div className="flex w-full flex-col items-center gap-2">
-          <progress className="progress progress-error h-6" value={80} max="100"></progress>
-          <div className="text-md font-bold text-gray-600">
+          <progress className={`progress progress-error ${sizeClass.progressHeight}`} value={80} max="100"></progress>
+          <div className={`font-bold text-gray-600 ${sizeClass.textSize}`}>
             <span className="text-error">80p</span>
             <span>&nbsp;/&nbsp;</span>
             <span>100p</span>
@@ -26,8 +32,8 @@ export function ActivityMissionZone({ title }: IProps) {
         <Image
           className="absolute right-[-13px] top-[-32%] z-10"
           src={'/lv2.svg'}
-          width={50}
-          height={50}
+          width={sizeClass.imgSize}
+          height={sizeClass.imgSize}
           alt="lv1 캐릭터"></Image>
       </div>
     </div>
